a1ea18b836b6a56943cff16d5c21f86265b3ee42
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-03-18 18:28:18 +0600

    add method for likeRepository.

--- src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/LikesRepository.java
+++ src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/LikesRepository.java
@@ -1,4 +1,10 @@
 package com.example.java_group_11_homework_51_ayday_mirbekkyzy.Repository;
 
+import com.example.java_group_11_homework_51_ayday_mirbekkyzy.Model.Post;
+
 public interface LikesRepository {
+//    public void likeOfPosts(Post post){
+//        для подсчета лайков данного поста
+//    }
+
 }
