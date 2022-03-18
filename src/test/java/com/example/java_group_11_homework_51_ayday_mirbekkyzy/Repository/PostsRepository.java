76f6d79e02c0431587f762698121d7e301220266
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-03-18 18:28:32 +0600

    add method for PostsRepository.

--- src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/PostsRepository.java
+++ src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/PostsRepository.java
@@ -1,4 +1,13 @@
 package com.example.java_group_11_homework_51_ayday_mirbekkyzy.Repository;
 
+import java.util.List;
+
 public interface PostsRepository {
+//   public  List<Post> findByAllPost (User user){
+//   получение всех постов юзера на странице
+//   }
+//   public List<Post> findPost( Following follower){
+//   для указания постов подписчиков и подписок
+//   }
+
 }
