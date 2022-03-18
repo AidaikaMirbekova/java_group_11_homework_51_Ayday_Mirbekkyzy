8466b7baf567b5817d2c0baeb1165cb80e59a12d
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-03-18 18:38:49 +0600

    add method for FollowRepository.

--- src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/FollowingRepository.java
+++ src/test/java/com/example/java_group_11_homework_51_ayday_mirbekkyzy/Repository/FollowingRepository.java
@@ -1,4 +1,15 @@
 package com.example.java_group_11_homework_51_ayday_mirbekkyzy.Repository;
 
 public interface FollowingRepository {
+
+    public void findFollowers(){
+        поиск по id своих подписчиков
+        для установления на поле подписчки всех фолловеров
+    }
+
+    public void findFollowing(){
+        поиск по id своих подписок
+        для установления на поле подписки своих подписок
+        и для пометки как булево для избежания повторного подписки
+    }
 }
